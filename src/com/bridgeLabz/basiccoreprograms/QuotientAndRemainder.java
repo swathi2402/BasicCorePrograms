d7a223323b4ff9adbd39b2a919cd1303a2fab199
Swathi <swathi.h@perfios.com>
2021-08-22 18:06:32 +0530

    ADDED: Class QuotientAndRemainder to compute Quotient and Remainder

---
+++ src/com/bridgeLabz/basiccoreprograms/QuotientAndRemainder.java
@@ -0,0 +1,14 @@
+package com.bridgeLabz.basiccoreprograms;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+
+	    int dividend = 50;
+	    int divisor = 4;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+}
