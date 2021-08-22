5fad539884057dece55976ec9bceb91b9c3f1ce3
Swathi <swathi.h@perfios.com>
2021-08-22 18:19:48 +0530

    ADDED: Class EvenOrOdd to check Whether a Number is Even or Odd

---
+++ src/com/bridgeLabz/basiccoreprograms/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package com.bridgeLabz.basiccoreprograms;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		int number = 22;
+		if(number % 2 == 0)
+            System.out.println(number + " is even.");
+        else
+            System.out.println(number + " is odd.");
+
+    }
+}
