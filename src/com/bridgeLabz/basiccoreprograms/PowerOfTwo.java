689fd3daa607e8229c3d170d2db8f671a7114d66
Swathi <swathi.h@perfios.com>
2021-08-23 08:05:25 +0530

    REFACTORED: Added logic of power zero to class PowerOfTwo

--- src/com/bridgeLabz/basiccoreprograms/PowerOfTwo.java
+++ src/com/bridgeLabz/basiccoreprograms/PowerOfTwo.java
@@ -5,14 +5,18 @@ public class PowerOfTwo {
 		int number = Integer.parseInt(args[0]);
 		int power = 1;
 		System.out.println("Table of powers of 2 of number " + number);
-		if(number > 0 && number < 31) {
+		if(number >= 0 && number < 31) {
+			if (number == 0) {
+				System.out.println("2 ^ " + number + " is " + power);
+			}
 			for (int index = 1; index <= number; index++) {
 				power *= 2;
 				System.out.println("2 ^ " + index + " is " + power);
 			}
 		}
+		
 		else {
-			System.out.println("Number should be between 1 and 31");
+			System.out.println("Number should be between 0 and 31");
 		}
 	}
 }
